b38dd39b0546ad1135a1054f6be871c8fc1f8242
Francesco <cesareo.francesco@gmail.com>
2025-05-06 15:59:44 +0200

    fix sonar

--- src/main/java/it/gov/pagopa/standinmanager/controller/InternalController.java
+++ src/main/java/it/gov/pagopa/standinmanager/controller/InternalController.java
@@ -13,7 +13,6 @@ import java.time.ZonedDateTime;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
